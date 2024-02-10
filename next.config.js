36e5c8457a393d69ee2f812bc04fdf62ed5907f7
Mốc Meo <minh@xuvi.vn>
2024-02-10 11:14:05 +0700

    Change to nextjs
    Signed-off-by: Mốc Meo <minh@xuvi.vn>

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra({
+  // images: {
+  //   unoptimized: true,
+  // },
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+});
---
+++ theme.config.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: "https://github.com/dapxexuyenviet/dapxexuyenviet.github.io",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase:
+    "https://github.com/dapxexuyenviet/dapxexuyenviet.github.io",
+  footer: {
+    text: "Nextra Docs Template",
+  },
+};
+
+export default config;
