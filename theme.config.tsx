84336df96806d4560ac4ac900fab37df78162125
Minh Mốc Meo <minh@xuvi.vn>
2024-02-12 00:36:18 +0700

    Update SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,8 +14,28 @@ const config: DocsThemeConfig = {
   useNextSeoProps() {
     return {
       titleTemplate: "%s – MocMeo",
+      canonical: "https://minh.xuvi.vn/",
+      twitter: {
+        cardType: "summary_large_image",
+        site: "@minhxuvi",
+        handle: "@minhxuvi",
+      },
     };
   },
+  head: (
+    <>
+      <html lang="vi"></html>
+      <meta name="msapplication-TileColor" content="#fff" />
+      <meta http-equiv="Content-Language" content="vi" />
+      <meta
+        name="description"
+        content="Minh Mốc Meo: Father, Engineer, Wanderer"
+      />
+      <meta property="og:title" content="Minh Mốc Meo" />
+      <meta property="og:description" content="Father, Engineer, Wanderer" />
+      <meta name="apple-mobile-web-app-title" content="Mốc Meo" />
+    </>
+  ),
 };
 
 export default config;
